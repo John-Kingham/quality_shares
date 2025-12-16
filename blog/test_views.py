6e713ec5b54804a7b98393a2152eaae9840f56b1
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-12-16 14:11:33 +0000

    test: add category list tests

--- blog/test_views.py
+++ blog/test_views.py
@@ -2,56 +2,74 @@ from http import HTTPStatus
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
-from .models import Post
+from .models import Category, Post
 
 
 class TestPostViews(TestCase):
 
     def setUp(self):
-        # create two posts, one published, one draft.
+        """
+        Set up two posts, one published, one draft, each attached to a
+        different category.
+        """
+        # Set up two categories
+        self.category_with_published_post = Category(name="Category 1")
+        self.category_with_published_post.save()
+        self.category_no_published_post = Category(name="Category 2")
+        self.category_no_published_post.save()
+
+        # Set up a published post
         self.user = User.objects.create_superuser(
             username="test-username",
             password="test-password",
             email="text@example.com",
         )
-        self.published_title = "Published Post Title"
-        self.published_description = "Published Post Description"
-        self.published_content = "Published Post Content"
-        self.published_slug = "published-post-slug"
         self.published_post = Post(
             author=self.user,
             published=True,
-            title=self.published_title,
-            description=self.published_description,
-            content=self.published_content,
-            slug=self.published_slug,
+            category=self.category_with_published_post,
+            title="Published Post Title",
+            description="Published Post Description",
+            content="Published Post Content",
+            slug="published-post-slug",
         )
         self.published_post.save()
-        self.draft_title = "Draft Post Title"
+
+        # Set up a draft post
         self.draft_post = Post(
             author=self.user,
-            title=self.draft_title,
+            title="Draft Post Title",
+            category=self.category_no_published_post,
         )
         self.draft_post.save()
-
-    def tearDown(self):
-        self.user = None
-        self.published_post = None
-        self.draft_post = None
 
     def test_post_list_view(self):
         """Test that the post list page contains the correct information"""
         response = self.client.get(reverse("post_list"))
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertContains(response, self.published_title)
-        self.assertContains(response, self.published_description)
-        self.assertContains(response, self.published_slug)
-        self.assertNotContains(response, self.draft_title)
+        self.assertContains(response, self.published_post.title)
+        self.assertContains(response, self.published_post.description)
+        self.assertContains(response, self.published_post.slug)
+        self.assertContains(response, self.published_post.category.name)
+        self.assertNotContains(response, self.draft_post.title)
+        self.assertNotContains(response, self.draft_post.category.name)
 
     def test_post_detail_view(self):
         """Test that the post detail page contains the correct information"""
-        post_detail_url = reverse("post_detail", args=[self.published_slug])
+        post_detail_url = reverse(
+            "post_detail", args=[self.published_post.slug]
+        )
         response = self.client.get(post_detail_url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertContains(response, self.published_title)
-        self.assertContains(response, self.published_content)
+        self.assertContains(response, self.published_post.title)
+        self.assertContains(response, self.published_post.content)
+        self.assertContains(response, self.published_post.category.name)
+
+    def test_category_list_view(self):
+        """Test that the category list (home) page is rendering correctly"""
+        response = self.client.get(reverse("category_list"))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, self.published_post.title)
+        self.assertContains(response, self.published_post.category.name)
+        self.assertNotContains(response, self.draft_post.title)
+        self.assertNotContains(response, self.draft_post.category.name)
