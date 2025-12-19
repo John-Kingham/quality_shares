91ecaf5c88385eb0a36ed59edc930e554987370f
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-12-19 14:32:03 +0000

    feat: add about template

--- about/views.py
+++ about/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import About
 
 
-def test_view(request):
-    return HttpResponse("The About page is working.")
+def about_view(request):
+    """
+    Returns a response for the About page.
+
+    Args:
+        request (HttpRequest): A GET request.
+
+    Returns:
+        HttpResponse: Contains the About page.
+    """
+    about = About.objects.first()
+    context = {"about": about}
+    return render(request, "about/about.html", context)
